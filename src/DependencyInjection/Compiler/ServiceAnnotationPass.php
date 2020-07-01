5f97448a47e1a3851af43c43ec0c5537e66c6bab
Andreas Schempp <andreas.schempp@terminal42.ch>
2020-07-01 20:13:59 +0200

    Fix the license info

--- src/DependencyInjection/Compiler/ServiceAnnotationPass.php
+++ src/DependencyInjection/Compiler/ServiceAnnotationPass.php
@@ -3,9 +3,7 @@
 declare(strict_types=1);
 
 /*
- * Password Validation Bundle for Contao Open Source CMS.
- *
- * @copyright  Copyright (c) 2019, terminal42 gmbh
+ * @copyright  Copyright (c) 2020, terminal42 gmbh
  * @author     terminal42 gmbh <https://terminal42.ch>
  * @license    MIT
  * @link       http://github.com/terminal42/service-annotation-bundle
