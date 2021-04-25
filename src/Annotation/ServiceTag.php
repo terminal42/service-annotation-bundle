470334adf3f5e5044f96e7353a89dedb2bf3399b
M. Vondano <m-vo@users.noreply.github.com>
2021-04-25 18:27:24 +0200

    add \Attribute to ServiceTag class and introduce BC layer in constructor

--- src/Annotation/ServiceTag.php
+++ src/Annotation/ServiceTag.php
@@ -22,6 +22,7 @@ use Doctrine\Common\Annotations\Annotation\Target;
  *     @Attribute("value", required = true, type = "string"),
  * })
  */
+#[\Attribute(\Attribute::IS_REPEATABLE | \Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD)]
 class ServiceTag implements ServiceTagInterface
 {
     /**
@@ -34,13 +35,27 @@ class ServiceTag implements ServiceTagInterface
      */
     protected $attributes = [];
 
-    public function __construct(array $data)
+    /**
+     * @param string|array $name
+     */
+    public function __construct($name, array $data = null)
     {
-        $this->name = $data['value'];
+        if (\is_array($name) && null === $data) {
+            // Instantiated by annotation reader
+            $data = $name;
 
-        unset($data['value']);
+            $this->name = $data['value'];
+            unset($data['value']);
+        } else {
+            // Instantiated using attributes
+            if (!\is_string($name)) {
+                throw new \InvalidArgumentException('Name must be a string.');
+            }
 
-        $this->attributes = $data;
+            $this->name = $name;
+        }
+
+        $this->attributes = $data ?? [];
     }
 
     public function getName(): string
